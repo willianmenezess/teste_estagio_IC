557a6814f76a851ccf0c6f77604be67a6da91c69
Willian Menezes da Silva <willmenezes@outlook.com.br>
2025-03-26 12:37:32 -0300

    feat: cria funcao download_pfds

--- teste_nivelamento.py
+++ teste_nivelamento.py
@@ -2,21 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_title(url):
-    # Envia uma requisição HTTP para o site
     response = requests.get(url)
-    
-    # Verifica se a requisição foi bem-sucedida (código 200)
     if response.status_code == 200:
-        # Cria um objeto BeautifulSoup para parsear o conteúdo HTML
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Obtém e retorna o título da página
         title = soup.find('title').text if soup.find('title') else 'Título não encontrado'
         return title
     else:
         return f"Erro ao acessar a página. Status: {response.status_code}"
 
+def download_pdfs(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pdf_links = [a['href'] for a in soup.find_all('a', href=True) if 'anexo' in a['href'].lower() and a['href'].endswith('.pdf')]
+        
+        for i, pdf_link in enumerate(pdf_links, 1):
+            pdf_url = pdf_link if pdf_link.startswith('http') else url + '/' + pdf_link
+            pdf_response = requests.get(pdf_url)
+            if pdf_response.status_code == 200:
+                filename = f"Anexo_{i}.pdf"
+                with open(filename, 'wb') as file:
+                    file.write(pdf_response.content)
+                print(f"Download concluído: {filename}")
+            else:
+                print(f"Erro ao baixar {pdf_url}")
+    else:
+        print(f"Erro ao acessar a página. Status: {response.status_code}")
+
 if __name__ == '__main__':
-    # URL do site
     url = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
     print(get_title(url))
+    download_pdfs(url)
+
